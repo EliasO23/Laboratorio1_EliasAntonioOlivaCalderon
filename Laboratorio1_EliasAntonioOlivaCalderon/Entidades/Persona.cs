21e9a0a053ff12035511a0f1dfb46e246e5143a3
EliasO23 <124007307+EliasO23@users.noreply.github.com>
2023-03-01 20:17:16 -0600

    Proyecto de Laboratorio

---
+++ Laboratorio1_EliasAntonioOlivaCalderon/Entidades/Persona.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio1_EliasAntonioOlivaCalderon.Entidades
+{
+    public class Persona
+    {
+        public String Nombre {get;set;}
+        public String Apellido {get; set;}
+        public int Edad {get;set;}
+        public char Sexo {get;set;}
+        public double Peso {get;set;}
+        public double Altura{get;set;}
+    }
+}
---
+++ Laboratorio1_EliasAntonioOlivaCalderon/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Laboratorio1_EliasAntonioOlivaCalderon.Negocio;
+using Laboratorio1_EliasAntonioOlivaCalderon.Entidades;
+
+Persona persona = new Persona();
+ClsPersona clsPersona = new ClsPersona();
+
+
+Console.WriteLine("\nIngrese su nombre:");
+persona.Nombre = Console.ReadLine();
+Console.WriteLine("Ingrese su apellido:");
+persona.Apellido = Console.ReadLine();
+Console.WriteLine("Ingrese su edad:");
+persona.Edad = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese su sexo (H = Hombre M = Mujer):");
+persona.Sexo = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Ingrese su peso (Kg):");
+persona.Peso = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingrese su altura (m):");
+persona.Altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\n------------------------------");
+Console.WriteLine("Informacion: ");
+Console.WriteLine("------------------------------");
+Console.WriteLine($"Paciente: {persona.Nombre} {persona.Apellido}");
+Console.WriteLine($"Edad: {persona.Edad}");
+Console.WriteLine($"Sexo: {persona.Sexo}");
+Console.WriteLine($"Peso: {persona.Peso}");
+Console.WriteLine($"Altura: {persona.Altura}");
+Console.WriteLine("------------------------------");
+Console.WriteLine($"Paciente: {persona.Nombre} {persona.Apellido}");
+Console.WriteLine(clsPersona.CalcularIMC(persona));
+Console.WriteLine(clsPersona.EsMayordeEdad(persona));
+Console.WriteLine("------------------------------\n");
+
